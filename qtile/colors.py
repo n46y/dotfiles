48d20459518b302b08344c58494694c0c55dd3cf
n46y <nagy.gusztav.work@gmail.com>
2023-02-01 12:07:39 +0100

    init

---
+++ qtile/colors.py
@@ -0,0 +1,19 @@
+def catppuccin():
+    colors = [
+            ["#1e1e2e", "#1e1e2e"], #background
+            ["#bac2de", "#bac2de"], #subtext
+            ["#cdd6f4", "#cdd6f4"], #text
+            ["#89b4fa", "#89b4fa"], #blue
+            ["#94e2d5", "#94e2d5"], #teal
+            ["#a6e3a1", "#a6e3a1"], #green
+            ["#fab387", "#fab387"], #peach
+            ["#f5c2e7", "#f5c2e7"], #pink
+            ["#cba6f7", "#cba6f7"], #purple
+            ["#f38ba8", "#f38ba8"], #red
+            ["#f9e2af", "#f9e2af"] #yellow
+            ]
+    backgroundColor = "#1e1e2e"
+    foregroundColor = "#cdd6f4"
+    workspaceColor = "#94e2d5"
+    foregroundColorTwo = "#bac2de"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
