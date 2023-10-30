40ff8d23c284d11384b3a25c4f171aebf1ec0225
n46y <nagy.gusztav.work@gmail.com>
2023-10-30 09:10:03 +0100

    oopsie tmux nav fixed

--- neovim/custom/mappings.lua
+++ neovim/custom/mappings.lua
@@ -6,11 +6,7 @@ M.general = {
     ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
     ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-  },
-  -- v = {
-  --   ["C-j"] = {":m '>+1<CR>gv=gv", "move line down"},
-  --   ["C-k"] = {":m '<-2<CR>gv=gv", "move line up" },
-  -- },
+  }
 }
 
 return M
